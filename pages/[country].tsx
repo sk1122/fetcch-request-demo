044e53f3fc49fd4b975b94b69b49432f4fd406e3
Satyam Kulkarni <punekar.satyam@gmail.com>
2023-04-04 14:53:05 +0530

    good

--- pages/[country].tsx
+++ pages/[country].tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import type { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import type { ParsedUrlQuery } from 'querystring'
@@ -10,6 +10,17 @@ import map from '../public/map.svg'
 import api from '../api'
 import { PRODUCT_PRICE } from '../constants'
 import { getParityPrice } from '../utils'
+import toast from 'react-hot-toast'
+import { Ethereum } from '../components/icons/ethereum'
+import { Polygon } from '../components/icons/polygon'
+import { Bnb } from '../components/icons/bnb'
+import { Avax } from '../components/icons/avax'
+import { Optimism } from '../components/icons/optimism'
+import { Arbitrium } from '../components/icons/arbitrium'
+import { Solana } from '../components/icons/solana'
+import { Aptos } from '../components/icons/aptos'
+import SelectDemo from '../components/ui/select'
+import { ethers } from 'ethers'
 
 interface Params extends ParsedUrlQuery {
   country: Country
@@ -43,12 +54,173 @@ export const getStaticProps: GetStaticProps<unknown, Params> = async ({
   }
 }
 
+export const chainsList = [
+  {
+    id: 1,
+    chainId: 1,
+    name: "Ethereum Mainnet",
+    token: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    tokenName: "ETH",
+    logo: Ethereum,
+    decimals: 18,
+    blockExplorer: "https://etherscan.io",
+    selected: false,
+    type: 'EVM'
+  },
+  {
+    id: 2,
+    chainId: 137,
+    name: "Polygon Mainnet",
+    token: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    tokenName: "MATIC",
+    logo: Polygon,
+    decimals: 18,
+    blockExplorer: "https://polygonscan.io",
+    selected: false,
+    type: 'EVM'
+  },
+  {
+    id: 3,
+    chainId: 56,
+    name: "BSC Mainnet",
+    token: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    tokenName: "BNB",
+    logo: Bnb,
+    decimals: 18,
+    blockExplorer: "https://bscscan.io",
+    selected: false,
+    type: 'EVM'
+  },
+  {
+    id: 4,
+    chainId: 43114,
+    name: "Avalanche C-Chain Mainnet",
+    token: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    tokenName: "AVAX",
+    logo: Avax,
+    decimals: 18,
+    blockExplorer: "https://snowtrace.io",
+    selected: false,
+    type: 'EVM'
+  },
+  {
+    id: 5,
+    chainId: 10,
+    name: "Optimism Mainnet",
+    token: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    tokenName: "ETH",
+    logo: Optimism,
+    decimals: 18,
+    blockExplorer: "https://optimistic.etherscan.io",
+    selected: false,
+    type: 'EVM'
+  },
+  {
+    id: 6,
+    chainId: 42161,
+    name: "Arbitrium Mainnet",
+    token: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    tokenName: "ETH",
+    decimals: 18,
+    logo: Arbitrium,
+    blockExplorer: "https://arbiscan.io",
+    selected: false,
+    type: 'EVM'
+  },
+  {
+    id: 7,
+    chainId: 7,
+    name: "Solana Mainnet",
+    token: "11111111111111111111111111111111111111111111",
+    tokenName: "SOL",
+    logo: Solana,
+    decimals: 9,
+    blockExplorer: "https://solana.fm",
+    selected: false,
+    type: 'SOLANA'
+  },
+  {
+    id: 8,
+    chainId: 8,
+    name: "Aptos Mainnet",
+    token: "0x1::aptos_coin::AptosCoin",
+    tokenName: "APT",
+    decimals: 8,
+    logo: Aptos,
+    blockExplorer: "https://aptoscan.io",
+    selected: false,
+    type: 'APTOS'
+  },
+];
+
 export default function CountryPage({ country, parity }) {
-  const [isParityEnabled, toggleParity] = useState<boolean>(false)
-  const parityPrice = useMemo(
-    () => getParityPrice(PRODUCT_PRICE, parity),
-    [parity]
-  )
+  const [parityPrice, setParityPrice] = useState(0.00000196)
+  const [chain, setChain] = useState(chainsList)
+  const [selected, setSelected] = useState(chainsList[0])
+  const [fetcchId, setFetcchId] = useState("")
+  const [message, setMessage] = useState("")
+  const [requestId, setRequestId] = useState(0)
+  const [toastId, setToastId] = useState("")
+
+  useEffect(() => {
+
+    if(requestId) {
+      const interval = setInterval(async () => {
+        const res = await fetch(`https://sandbox-api.fetcch.xyz/v1/request/?id=${requestId}`, {
+          headers: {
+          'secret-key': "87e4153d-bfc2-4880-a034-d614f2626103",
+          'content-type': 'application/json'
+        }})
+
+        const data = await res.json()
+        if(data.data[0].executed) {
+          toast.success(`Successfully resolved payment request - ${data.data[0].transactionHash}`, {
+            id: toastId
+          })
+        }
+      }, 1000)
+    
+      return () => clearInterval(interval)
+    }
+  }, [requestId])
+
+  const requestPayment = async () => {
+    if(fetcchId) {
+      if(fetcchId.split("@").length < 2) {
+        toast.error("Wrong Fetcch ID")
+        return
+      } 
+      
+      const res = await fetch("https://sandbox-api.fetcch.xyz/v1/request/", {
+        method: 'POST',
+        body: JSON.stringify({
+          "payer": fetcchId,
+          "receiver": "wag@fetcch",
+          "amount": ethers.utils.parseUnits(parityPrice.toString(), selected.decimals).toString(),
+          "token": selected?.token,
+          "chain": selected?.id,
+          "message": message,
+          "label": "Product Name3"
+        }),
+        headers: {
+          'secret-key': "87e4153d-bfc2-4880-a034-d614f2626103",
+          'content-type': 'application/json'
+        }
+      })
+
+      const data = await res.json()
+
+      setRequestId(data.data.id)
+
+      const id = toast.loading("Checking payment request")
+
+      setToastId(id)
+
+      toast.success(`Successfully request ${parityPrice.toString()} ${selected.tokenName}`)
+    } else {
+      toast.error("Enter Fetcch ID first")
+    }
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-10 bg-gray-50">
@@ -62,17 +234,17 @@ export default function CountryPage({ country, parity }) {
         />
       </div>
       <main className="flex flex-col items-center flex-1 px-4 sm:px-20 text-center z-10 sm:pt-10">
-        <h1 className="text-3xl sm:text-5xl font-bold">Power parity pricing</h1>
+        <h1 className="text-3xl sm:text-5xl font-bold">Fetcch Payment Request Demo</h1>
         <p className="mt-4 sm:text-xl text-lg text-gray-700">
-          Show localized pricing based on location headers
+          Buy this using any cryptocurrency!!
         </p>
         <a
           className="flex items-center mt-4 text-md sm:text-lg text-blue-500 hover:underline"
-          href="https://vercel.com/docs/edge-network/headers#request-headers?utm_source=geo-ip-demo&utm_campaign=geo-ip-demo"
+          href="https://docs.fetcch.xyz"
           target="_blank"
           rel="noreferrer"
         >
-          View headers documentation
+          View Fetcch documentation
           <svg
             viewBox="0 0 24 24"
             width="16"
@@ -104,50 +276,18 @@ export default function CountryPage({ country, parity }) {
                 <h4 className="font-semibold text-xl">Alpha Black shirt</h4>
                 <h5 className="text-gray-700">Limited edition</h5>
               </div>
-              {isParityEnabled ? (
-                <div className="flex flex-col items-start font-bold text-lg leading-none">
-                  <span className="text-gray-500 text-sm line-through">
-                    USD {PRODUCT_PRICE}
-                  </span>
-                  <span className="text-green-500">USD {parityPrice}</span>
-                </div>
-              ) : (
-                <h4 className="font-bold text-lg">USD {PRODUCT_PRICE}</h4>
-              )}
+              <h4 className="font-bold text-lg">{selected.tokenName} {parityPrice}</h4>
             </div>
           </div>
+          <div className="z-50 p-4 space-y-3 flex flex-col justify-center items-center border-b">
+            <input value={fetcchId} onChange={(e) => setFetcchId(e.target.value)} className='w-full border p-3' placeholder='Your Fetcch ID' />
+            <SelectDemo chains={chainsList} selected={selected} setSelected={setSelected} />
+          </div>
           <div className="p-4 gap-4 flex flex-col justify-center items-center border-b">
-            <div className="bg-gray-50 text-gray-500 text-left py-2 px-4 rounded-md border-gray-200 border text-sm flex flex-col gap-4">
-              <p className="inline-block">
-                <span>We noticed that you&apos;re from </span>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  className="bg-gray-200 inline-flex"
-                  width={16}
-                  height={12}
-                  alt={`Country flag for ${country.toUpperCase()}`}
-                  src={`https://country-flags.vercel.sh/s/${country.toUpperCase()}.svg`}
-                />
-                <span>
-                  . We are offering purchasing power parity pricing. If that is
-                  something that you need:
-                </span>
-              </p>
-              <label className="inline-flex items-center font-semibold">
-                <input
-                  onChange={(event) => toggleParity(event.target.checked)}
-                  className="text-black-500 w-4 h-4 mr-2 border border-gray-300 rounded"
-                  type="checkbox"
-                />
-                Activate {parity}% off with regional pricing
-              </label>
-            </div>
             <button
               className="py-4 px-6 text-lg w-full bg-black text-white rounded-md hover:bg-gray-900"
               onClick={() =>
-                alert(
-                  `its yours for USD ${isParityEnabled ? parityPrice : 500}`
-                )
+                requestPayment()
               }
             >
               Buy now
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,20 @@ import type { AppProps } from 'next/app'
 import type { LayoutProps } from '@vercel/examples-ui/layout'
 import { getLayout } from '@vercel/examples-ui'
 import '@vercel/examples-ui/globals.css'
+import { Toaster } from 'react-hot-toast'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component)
 
   return (
-    <Layout
-      title="Power parity pricing"
-      path="edge-middleware/power-parity-pricing"
-    >
+    // <Layout
+    //   title="Power parity pricing"
+    //   path="edge-middleware/power-parity-pricing"
+    // >
+    <>
+      <Toaster position="top-right" />
       <Component {...pageProps} />
-    </Layout>
+    </>
+    // </Layout>
   )
 }
